8aa166dd0506338a03f026fdd8751d162ae85c1f
srivatsan <vatsan_k@hotmail.com>
2021-11-25 17:08:04 +0530

    Updated at the End of the day

---
+++ api-gateway/src/main/java/com/example/demo/filters/MyGlobalProxyFilter.java
@@ -0,0 +1,30 @@
+package com.example.demo.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyGlobalProxyFilter implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+	
+		
+		boolean result =
+				exchange.getRequest().getURI().
+				        getRawPath().contains("message");
+				      
+				   if(result) {
+				exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+				        return exchange.getResponse().setComplete();
+				        }
+				      
+				return chain.filter(exchange);
+	}
+
+}
