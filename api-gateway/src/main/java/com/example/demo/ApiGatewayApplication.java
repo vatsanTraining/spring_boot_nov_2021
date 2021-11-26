a2e4cc1457f7390cfe167bc671b52124688e2de4
srivatsan <vatsan_k@hotmail.com>
2021-11-26 19:03:45 +0530

    API Gateway With Authentication Filter Added

--- api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,15 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public WebClient.Builder builRef(){
+		
+		return WebClient.builder();
+	}
+	
+	@Bean
+	public WebClient restClient( WebClient.Builder builRef) {
+		
+		return builRef.build();
+	}
 }
---
+++ api-gateway/src/main/java/com/example/demo/filters/AuthFilter.java
@@ -0,0 +1,50 @@
+package com.example.demo.filters;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter {
+
+	@Autowired
+    private  WebClient webClientBuilder;
+
+	
+	 
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            throw new RuntimeException("Missing authorization information");
+        }
+
+        List<String> authorization = exchange.getRequest().getHeaders().get("Authorization");
+
+        
+
+         if(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0)!=null) {
+         
+
+        ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().
+        	      header("Authorization", authorization.get(0)).build();
+        	   
+        	     return chain.filter(exchange.mutate().request(modifiedRequest).build());
+         } else {
+        	 
+        	 return chain.filter(exchange);
+         }
+  
+	}
+	
+}
+
+
